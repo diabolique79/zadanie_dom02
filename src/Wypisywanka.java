31f1457ce74bd227580e50c781bc0ef6d47f0bbc
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-07 18:33:57 +0200

    zadanie-java-32

---
+++ src/Wypisywanka.java
@@ -0,0 +1,22 @@
+public class Wypisywanka {
+
+    public static void main(String[] args) {
+
+        System.out.println("Klasa to zestaw cech, szablon");
+        System.out.println("Obiekt to konkretna reprezantacja klasy (nadawane są konkretne cechy) i jest tworzony na podstawie klasy");
+        System.out.println("Zmienna to element przechowywania danych");
+        System.out.println("Każda zmienna musi mieć określony typ, np. int, albo String.");
+        System.out.println("W klasie mogą się znajdować konstruktory, metody i pola");
+        System.out.println("Klasy są wywoływane przez operator new i służą do tworzenia nowych obiektów.");
+        System.out.println("Jeśli w klasie zdefiniowano chociaż jeden konstruktor to kompilator nie wygeneruje dla nas kontruktora domyślnego.");
+        System.out.println("Zakładając, że istnieje klasa Car, to przy zapisie:");
+        System.out.println("Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);");
+        System.out.println("car1 nazywamy obiektem,");
+        System.out.println("a przy zapisie:");
+        System.out.println("Car car1 = new Car(„BMW”, „M5”, 2007, 2.6);  Car car2 = car1;");
+        System.out.println("car1 I car2 to dwie zmienne wskazujące na ten sam obiekt");
+
+
+    }
+    }
+
