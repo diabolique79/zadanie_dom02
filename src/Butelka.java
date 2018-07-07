15ec8cfdb636dffe7459d046202085f32fd8f521
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-07 18:19:21 +0200

    zadanie-java-2 - 1.2
    pliki Butelka i OpisButelki

--- src/Butelka.java
+++ src/Butelka.java
@@ -1,9 +1,31 @@
 public class Butelka {
 
+    int id;
     String name; //nazwa
     double capacity; //pojemnosc
     String type; // szklana, plastikowa
     String content; // woda, piwo, itp.
     int year; //rok producji
+
+    Butelka(int i, String n, double c, String t, String cn, int yr) {
+
+        id = i;
+        name = n;
+        capacity = c;
+        type = t;
+        content = cn;
+        year = yr;
+
+
+    }
+
+    void opiszButelke(){
+
+        System.out.println("Opis butelki nr " + id + ":");
+        System.out.println("Nazwa butelki: " + name + ", Pojemność: " + capacity + ", Rodzaj butelki: " + type + ", Zawartość i rok produkcji: " + content + " " + year);
+
+
+
+    }
 
 }
