8b3a0450dfbe96a925732ad5c448c3df0a2b30d8
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-07 16:23:58 +0200

    zadanie-java-31 - p1 Pliki class Cars, class Car

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car(String b, String m, int yr, double c) {
+        brand = b;
+        model = m;
+        year = yr;
+        capacity = c;
+    }
+
+
+}
